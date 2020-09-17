385e270703a7aff6cd31feda346cfa2820605273
root <root@localhost.localdomain>
2020-09-17 04:49:30 -0400

    renamed to underscore for import

---
+++ src/threat_intel.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+'''
+Incomplete, thinking of implementing a convertor to yara instead, then use yara to run the whole search, with segmented parts for each different 'protocol'
+'''
+
+import glob
+from util import Util
+from pprint import pformat
+from config import INTEL_DIR
+from features import extract_payload, find_streams
+from logger import logging, LOG_FILE, FORMATTER, TIMESTAMP
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(FORMATTER, TIMESTAMP)
+
+file_handler = logging.FileHandler(LOG_FILE)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+class Threat_intel:
+    def __init__(self):
+        self.intel_files = [fname for fname in glob.iglob(INTEL_DIR+"**/*.txt", recursive=True)] 
+        self.threat_ips = set()
+        self.threat_domains = set()
+
+    def run(self):
+        self.update()
+        pcap = Util.load_cap("sniffed")
+        stream_dict = find_streams(pcap)
+        
+        for k, stream in stream_dict.items():
+            #self.search(k)
+            print(stream)
+            exit(1)
+
+    def search(self, string):
+        for ip in self.threat_ips:
+            if ip in string:
+                print(ip)
+
+    def update(self):
+        for fname in self.intel_files:
+            if "domain" in fname.split("_")[2]:
+                with open(fname, 'r') as f:
+                    temp  = [line.strip() for line in f.readlines() if line[0] not in "#"]
+                    self.threat_domains.update(temp)
+
+            elif "ip" in fname.split("_")[2]:
+                with open(fname, 'r') as f:
+                    temp = [line.strip() for line in f.readlines()]
+                    self.threat_ips.update(temp)
+
+if __name__ == "__main__":
+    INTEL_DIR = "."+INTEL_DIR
+    threat = threat_intel()
+    #threat.update()
+    threat.run()
+
+
--- src/yara_process.py
+++ src/yara_process.py
@@ -3,17 +3,17 @@
 import os
 import glob
 import yara
+from config import RULES_DIR, CAP_PATH
 
 
 class YARA:
     def __init__(self):
-        self._rules_path = ""
         self._rules = ""
         self._matches = []
 
     # Loads in uncompiled rules files
     def load_rules(self):
-        rule_files = self._prepare_rules(self._rules_path)
+        rule_files = self._prepare_rules(RULES_DIR)
         
         # Compile all rules file in specified paths
         try:
@@ -24,18 +24,17 @@ class YARA:
     # Prepare uncompile yara rules files
     def _prepare_rules(self, rules):
         results = {}
-        for fname in glob.iglob(rules+"**/*.yar", recursive=True):
+        for fname in glob.iglob(RULES_DIR+"**/*.yar", recursive=True):
             with open(fname, 'r') as f:
                 results[os.path.basename(fname)[:-4]] = fname 
         return results
 
-    def run(self, rules_path):
-        self._rules_path = rules_path
+    def run(self):
         self.load_rules()
 
         # pcap matching
 #        matches = self._rules.match("./out3.pcap")
-        matches = self._rules.match("./testinlol")
+        matches = self._rules.match(CAP_PATH+"sniffed.cap")
 
 
         if matches:
@@ -45,8 +44,10 @@ class YARA:
         
 
 if __name__ == "__main__":
+    RULES_DIR = "."+RULES_DIR
+    CAP_PATH = "."+CAP_PATH
     yar = YARA()
-    yar.run("/root/Desktop/ICT2202_IOC/rules/")
+    yar.run()
     
 
 
