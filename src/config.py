eb476abdaccc81c090b10c4fe4206a900072cb90
root <root@localhost.localdomain>
2020-09-17 04:48:52 -0400

    added paths for rules & threat

--- src/config.py
+++ src/config.py
@@ -4,3 +4,9 @@ LOGGER_PATH = "./logs/"
 # Path to cap folder
 CAP_PATH = "./cap/"
 CAP_EXTENSION = ".cap"
+
+# Path to rules folder
+RULES_DIR = "./rules/"
+
+# Path to threat-intel folder
+INTEL_DIR = "./threat-intel/"
