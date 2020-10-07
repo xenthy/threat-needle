306c5d297bbe12e350c65e7c657e2666b1aca190
chong00 <chongpeng1996@gmail.com>
2020-10-08 02:06:12 +0800

    Added Stream Graph

--- src/static/js/traffic_chart_area.js
+++ src/static/js/traffic_chart_area.js
@@ -1,13 +1,14 @@
 var canvas = document.getElementById('myAreaChart');
 var data = {
-  labels: ["0", "1", "2", "3", "4", "5", "6", "7","8", "9"],
+  labels: ["00:00", "00:00", "00:00", "00:00", "00:00", "00:00", "00:00", "00:00", "00:00", "00:00"],
   datasets: [
     {
+      
       label: "Packets per second",
       fill: false,
       lineTension: 0.0,
       backgroundColor: "rgba(75,192,192,0.4)",
-      borderColor: "rgba(75,192,192,1)",
+      borderColor: "#4e73df",
       borderCapStyle: 'butt',
       borderDash: [],
       borderDashOffset: 0.0,
@@ -21,55 +22,99 @@ var data = {
       pointHoverBorderWidth: 2,
       pointRadius: 5,
       pointHitRadius: 10,
-      data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    }
+      data: [0, 0, 0, 0, 0 ,0, 0, 0, 0,0]
+    },
+    {
+      
+        label: "Streams per second",
+        fill: false,
+        lineTension: 0.0,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "#20c9a6",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 5,
+        pointHitRadius: 10,
+        data: [0, 0, 0, 0, 0 ,0, 0, 0, 0,0]
+      }
   ]
 };
 
+
 var prev = 0;
-var zero = 9;
+var prev_stream = 0;
+var time = "00:00";
+var a=0;
 var highest = null;
 var lowest = null;
-function adddata() {
-  highest = null;
-  lowest = null;
-  var value = document.getElementById("total_packets").innerText;
-  var temp = value;
+function add_data() {
+    highest = null;
+    lowest = null;
+    var pkts = document.getElementById("total_packets").innerText;
+    var stream_count = document.getElementById("total_streams").innerText;
+    var temp = pkts;
 
-  temp = (Number(value) - prev);
-  myLineChart.data.labels.push(zero);
-  myLineChart.data.labels.splice(0, 1);
-  myLineChart.data.datasets[0].data.splice(0, 1);
-  myLineChart.data.datasets[0].data.push(temp);
-
-  var data_list = myLineChart.data.datasets[0].data;
-
-  for (var i = 0; i < myLineChart.data.datasets[0].data.length; i++) {
-    if (lowest > data_list[i] || lowest == null) {
-      lowest = data_list[i];
-      // continue;
+    var str = myLineChart.data.labels.slice(-1)[0];
+    if(str.slice(-2) == "59"){
+        a = Number(str.slice(0,2)) + 1;
+        if(a < 10){
+            time = "0" + a + ":00";
+        }else{
+            time =  a + ":00";
+        }
+        
+    }else{
+        a = Number(str.slice(-2)) + 1;
+        if( a < 10){
+            time = str.slice(0,3) + "0" + a;
+        }else{
+            time = str.slice(0,3) + "" + a;
+        }
     }
+    myLineChart.data.labels.push(time);
+    myLineChart.data.labels.splice(0, 1);
 
-    if (highest < data_list[i] || highest == null) {
-      highest = data_list[i];
-      // continue;
+    temp = (Number(pkts) - prev);
+    myLineChart.data.datasets[0].data.splice(0, 1);
+    myLineChart.data.datasets[0].data.push(temp);
+
+    temp = (Number(stream_count) - prev_stream);
+    myLineChart.data.datasets[1].data.splice(0, 1);
+    myLineChart.data.datasets[1].data.push(temp);
+    
+    var data_list = myLineChart.data.datasets[0].data;
+
+    for (var i = 0; i < myLineChart.data.datasets[0].data.length; i++) {
+        // if (lowest > data_list[i] || lowest == null) {
+        // lowest = data_list[i];
+        // // continue;
+        // }
+
+        if (highest < data_list[i] || highest == null) {
+        highest = data_list[i];
+        // continue;
+        }
     }
-  }
-  console.log("highest: " + highest + " | lowest: " + lowest);
-  myLineChart.options.scales.yAxes[0].ticks.min = lowest;
-  myLineChart.options.scales.yAxes[0].ticks.max = highest;
-  myLineChart.update();
-  zero++;
-  prev = Number(value);
+   // myLineChart.options.scales.yAxes[0].ticks.min = lowest;
+    myLineChart.options.scales.yAxes[0].ticks.max = highest;
+    myLineChart.update();
+    prev = Number(pkts);
+    prev_stream = Number(stream_count);
   
 }
 
 
 
-setInterval(function () { adddata(); }, 2000);
-
-
-
+setInterval(function () { add_data(); }, 2000);
 var option = {
   layout:{
     padding:{
@@ -86,6 +131,7 @@ var option = {
         max: 1000
       }
     }]
+   
   }
 };
 
