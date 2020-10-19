4d0ef60fc7791de4c459c93a48509059269c28ed
ethancunt <eredflagy@gmail.com>
2020-10-19 14:08:30 -0400

    added comments

--- src/organize.py
+++ src/organize.py
@@ -1,5 +1,5 @@
-from vault import Vault
 from util import Util
+from vault import Vault
 from logger import logging, LOG_FILE, FORMATTER, TIMESTAMP
 
 logger = logging.getLogger(__name__)
@@ -25,7 +25,6 @@ class Organize:
         flagged_dict[timestamp] = flagged_details
 
         Vault.set_flagged(flagged_dict)
-        
         logger.info(f"Payload: {flag_matches} --> {yara_flagged[0].rule}")
 
     @staticmethod
@@ -39,13 +38,12 @@ class Organize:
         flagged_dict[timestamp] = flagged_details
 
         Vault.set_flagged(flagged_dict)
-
         logger.info(f"Threat: {flag_matches} --> {threat_flagged[0].rule}")
 
 
 class Payload_flagged:
     def __init__(self, stream_id, stream, payload, strings, rule, tags):
-        self.identifier = "stream"
+        self.identifier = "payload"
         self.stream_id = stream_id
         self.stream = stream 
         self.payload = payload
@@ -53,44 +51,58 @@ class Payload_flagged:
         self.rule = rule
         self.tags = tags
 
-    def get_stream_id(self):
-        return self.stream_id
-
+    # Identifier of this Object, steam or packet
     def get_identifier(self):
         return self.identifier
 
+    # Returns the stream's src and dst (stream header)
+    def get_stream_id(self):
+        return self.stream_id
+
+    # Returns the stream that yara flagged
     def get_stream(self):
         return self.stream
 
+    # Returns the payload extracted from the stream, that was flagged
     def get_payload(self):
         return self.payload
 
-    def get_strings(self):
+    # Returns the strings matched my yara 
+    # e.g. [(10L,'$a','xyz')]
+    def get_strings(self) -> list:
         return self.matched_strings
 
+    # Returns the rule name of the yara rule that was used to flag
     def get_rule(self):
         return self.rule
 
-    def get_tags(self):
+    # Returns the tag of the yara rule that was used to flag
+    def get_tags(self) -> list:
         return self.tags
 
 
 class Threat_flagged:
     def __init__(self, packet, strings, rule, tags):
-        self.identifier = "threat"
+        self.identifier = "endpoint"
         self.packet = packet
         self.strings = strings
         self.rule = rule
         self.tags = tags
 
+    # Identifier of this Object, steam or packet
     def get_identifier(self):
         return self.identifier
 
-    def get_strings(self):
+    # Returns the strings matched my yara 
+    # e.g. [(10L,'$a','xyz')]
+    def get_strings(self) -> list:
         return self.matched_strings
 
+    # Returns the rule name of the yara rule that was used to flag
     def get_rule(self):
         return self.rule
 
-    def get_tags(self):
+    # Returns the tag of the yara rule that was used to flag
+    # e.g. ['mail']
+    def get_tags(self) -> list:
         return self.tags
