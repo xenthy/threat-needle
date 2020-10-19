1b90117dfe1a0fac6aff9ef87dfb3bab7fa47c79
zen <zentan3118@gmail.com>
2020-10-19 14:35:29 +0800

    made simlation faster using threading
    Signed-off-by: zen <zentan3118@gmail.com>

--- simulate.py
+++ simulate.py
@@ -3,7 +3,24 @@
 import argparse
 import random
 import subprocess
+import threading
 
+
+def ip_worker(file, index):
+    while "#" in (random_ip := random.choice(file).strip()):
+        continue
+    subprocess.run(["ping", "-c", "1", "-w", "1", random_ip], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    print(f"[!] ip[{index + 1}] probed {random_ip}")
+
+
+def url_worker(file, index):
+    while "#" in (random_url := random.choice(file).strip()):
+        continue
+    subprocess.run(["nslookup", random_url], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    print(f"[!] url[{index + 1}] probed {random_url}")
+
+
+""" START """
 parser = argparse.ArgumentParser(description="Simulate threats",
                                  prog=__file__[2:])
 parser.add_argument("--ip", "-i",
@@ -31,20 +48,27 @@ if url < 0:
 if ip == 0 and url == 0:
     ip, url = 1, 1
 
+ip_threads, url_threads = [], []
+
 if ip != 0:
     with open("rules/threat_intel/malware_ip.txt") as f:
-        lines = f.readlines()
-        for _ in range(ip):
-            while "#" in (random_ip := random.choice(lines).strip()):
-                continue
-            subprocess.run(["ping", "-c", "1", "-w", "1", random_ip], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-            print(f"[!] probed {random_ip}")
+        file = f.readlines()
+    for index in range(ip):
+        thread = threading.Thread(target=ip_worker, args=(file, index,))
+        ip_threads.append(thread)
+        thread.start()
 
 if url != 0:
     with open("rules/threat_intel/malware_domain.txt") as f:
-        lines = f.readlines()
-        for _ in range(url):
-            while "#" in (random_url := random.choice(lines).strip()):
-                continue
-            subprocess.run(["nslookup", random_url], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-            print(f"[!] probed {random_url}")
+        file = f.readlines()
+    for index in range(url):
+        thread = threading.Thread(target=url_worker, args=(file, index,))
+        url_threads.append(thread)
+        thread.start()
+
+
+for t in ip_threads:
+    t.join()
+
+for t in url_threads:
+    t.join()
