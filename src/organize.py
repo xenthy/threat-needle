be7f13a7881d6e7cb09875c67f1883853791d30a
ethancunt <eredflagy@gmail.com>
2020-10-19 14:35:51 -0400

    removed redundant stuff

--- src/organize.py
+++ src/organize.py
@@ -16,10 +16,12 @@ class Organize:
     @staticmethod
     def add_stream_entry(stream_key, stream, stream_payload, yara_flagged, timestamp):
         flagged_dict = {}
-        # Timestamp here might be inaccurate, as its only getting the first timestamp of the stream (multiple packets)
         flag_matches = []
         for match in yara_flagged[0].strings:
-            flag_matches.append(match[2].decode('utf-8'))
+            try:
+                flag_matches.append(match[2].decode('utf-8'))
+            except:
+                flag_matches.append(match[2])
 
         flagged_details = Payload_flagged(stream_key, stream, stream_payload, yara_flagged[0].strings, yara_flagged[0].rule, yara_flagged[0].tags) 
         flagged_dict[timestamp] = flagged_details
@@ -45,41 +47,11 @@ class Payload_flagged:
     def __init__(self, stream_id, stream, payload, strings, rule, tags):
         self.identifier = "payload"
         self.stream_id = stream_id
-        self.stream = stream 
+        self.stream = stream
         self.payload = payload
         self.strings = strings
         self.rule = rule
         self.tags = tags
-
-    # Identifier of this Object, payload or endpoint
-    def get_identifier(self):
-        return self.identifier
-
-    # Returns the stream's src and dst (stream header)
-    def get_stream_id(self):
-        return self.stream_id
-
-    # Returns the stream that yara flagged
-    def get_stream(self):
-        return self.stream
-
-    # Returns the payload extracted from the stream, that was flagged
-    def get_payload(self):
-        return self.payload
-
-    # Returns the strings matched my yara 
-    # e.g. [(10L,'$a','xyz')]
-    def get_strings(self) -> list:
-        return self.matched_strings
-
-    # Returns the rule name of the yara rule that was used to flag
-    def get_rule(self):
-        return self.rule
-
-    # Returns the tag of the yara rule that was used to flag
-    def get_tags(self) -> list:
-        return self.tags
-
 
 class Threat_flagged:
     def __init__(self, packet, strings, rule, tags):
@@ -88,21 +60,3 @@ class Threat_flagged:
         self.strings = strings
         self.rule = rule
         self.tags = tags
-
-    # Identifier of this Object, payload or endpoint
-    def get_identifier(self):
-        return self.identifier
-
-    # Returns the strings matched my yara 
-    # e.g. [(10L,'$a','xyz')]
-    def get_strings(self) -> list:
-        return self.matched_strings
-
-    # Returns the rule name of the yara rule that was used to flag
-    def get_rule(self):
-        return self.rule
-
-    # Returns the tag of the yara rule that was used to flag
-    # e.g. ['mail']
-    def get_tags(self) -> list:
-        return self.tags
