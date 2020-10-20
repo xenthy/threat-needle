a26e3439f5adf2a66847b43d5ece062474faaea3
ethancunt <eredflagy@gmail.com>
2020-10-20 10:20:31 -0400

    integrated with Vault and queue stuff lmao

--- src/carver.py
+++ src/carver.py
@@ -1,42 +1,55 @@
-import random
 import re
-import string
-import io
-from io import BytesIO
-from config import CARVED_DIR
-
 import os
+import io
 import glob
 import yara
+import string
+import random
+from io import BytesIO
+from vault import Vault
+from config import SESSION_CACHE_PATH, CARVED_DIR
+
 
 class Carver:
     def __init__(self):
-        self.file_sigs = {"jgp":['255', '216'], "jpeg":['255','216'], "png":['137', '80'], "gif":['71', '73'], "pdf":['37', '80', '68', '70'], "docx":['80', '75', '3', '4']}
+        file_sigs = {"jgp":['255', '216'], "jpeg":['255','216'], "png":['137', '80'], "gif":['71', '73'], "pdf":['37', '80', '68', '70'], "docx":['80', '75', '3', '4']}
+
+
     def carve_stream(self, stream_payload):
-
-        b = BytesIO(stream_payload)
         magic = ""
-        cont_type, cont_length = self.get_content_info(b)
+        # cont_type, cont_length = self.get_content_info(b)
+        carving_queue= Vault.get_carving_queue()
 
-        for ft, mb in self.file_sigs.items():
-            if ft == cont_type:
-                magic = mb
-                break
-        
-        if cont_type and cont_length and magic:
-            SOF = self.get_SOF(magic, b)
-            if not SOF:
-                return 0
+        for k, timestamp, cont_type, cont_length in carving_queue:
+            with open(f"{SESSION_CACHE_PATH}/{k}",'rb') as f:
+                stream_payload = f.read()
+                
+            b = BytesIO(stream_payload)
+            for ft, mb in self.file_sigs.items():
+                if ft == cont_type:
+                    magic = mb
+                    break
             
-            EOF = SOF + cont_length
+            if cont_type and cont_length and magic:
+                SOF = self.get_SOF(magic, b)
+                if not SOF:
+                    return 0
+                
+                EOF = SOF + cont_length
 
-            view = b.getbuffer()
-            carved = view[SOF:EOF]
-            with open("../carved/test."+cont_type,'ab+') as f:
-                f.write(carved)
+                view = b.getbuffer()
+                carved = view[SOF:EOF]
 
-    def carve_packet(self, packet):
-        pass
+                with open(f"{SESSION_CACHE_PATH}/{Vault.get_runtime_name()}/{(fname := self.random_str)}."+cont_type,'ab+') as f:
+                    f.write(carved)
+                    Vault.add_carved_file(k, timestamp, f"{fname}.{cont_type}", cont_length)
+
+
+                
+                return carved
+
+    # def carve_packet(self, packet):
+        # pass
 
     def get_content_info(self, payload_b):
         cont_type  = ""
@@ -54,7 +67,7 @@ class Carver:
             if cont_type and cont_length:
                 return cont_type[0], int(cont_length[0])
 
-        return cont_type, cont_length
+        return None, None
         
 
     def get_SOF(self, magic, payload_b):
@@ -70,14 +83,12 @@ class Carver:
 
             print(''.join(magic),compare)
             if ''.join(magic) in compare:
-                print("FOUND")
                 return byte_count
 
             byte_count += 1
     
         return 0
     
-    # For testing only, will generate random filenames for carved files
     def random_str(self, length):
         letters = string.ascii_lowercase
         result = ''.join(random.choice(letters) for i in range(length))
--- src/yara_process.py
+++ src/yara_process.py
@@ -5,6 +5,8 @@
 '''
 
 from util import Util
+from io import BytesIO
+from vault import Vault
 from escapy import Escapy
 from carver import Carver
 from thread import Thread
@@ -30,65 +32,86 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
 
+## TODO: CHANGE TO STATIC METHOD
 class Yara:
-    def __init__(self):
-        self._rules = ""
-        self._url_rules = ""
-        self._matches = []
-        self.udp_ports = ["53", "80", "443"]
+    _rules = ""
+    _url_rules = ""
+    _matches = []
+    udp_ports = ["53", "80", "443"]
 
     # Loads in uncompiled rules files
-    def load_rules(self):
-        rule_files = self._prepare_rules(RULES_DIR)
-        url_rule_files = self._prepare_rules(INTEL_DIR)
+    @staticmethod
+    def load_rules():
+        rule_files = Yara._prepare_rules(RULES_DIR)
+        url_rule_files = Yara._prepare_rules(INTEL_DIR)
         # Compile all rules file in specified paths
         try:
-            self._rules = yara.compile(filepaths=rule_files)
-            self._url_rules = yara.compile(filepaths=url_rule_files)
+            Yara._rules = yara.compile(filepaths=rule_files)
+            Yara._url_rules = yara.compile(filepaths=url_rule_files)
         except Exception as e:
             logger.error(f"Invalid Rule file/syntax error: \n{e} [{Thread.name()}]")
-            #print(f"Invalid Rule file/syntax error: \n{e}")
 
     # Prepare uncompile yara rules files
-    def _prepare_rules(self, rules):
+    @staticmethod
+    def _prepare_rules(rules):
         results = {}
         for fname in glob.iglob(RULES_DIR+"**/*.yar", recursive=True):
             with open(fname, 'r') as f:
                 results[os.path.basename(fname)[:-4]] = fname
         return results
 
-    def run(self, stream_dict):
-        self.load_rules()
+    @staticmethod
+    def run(stream_dict):
+        Yara.load_rules()
         matches = None
         carver = Carver()
 
         for k, stream in stream_dict.items():
             if (payload := extract_payload(stream)) is not None:
-                carver.carve_stream(payload) # Testing for carving files from stream payload
+                raw_timestamp = Escapy.convert_packet(stream[0], "Timestamp")
+                timestamp = str(datetime.datetime.utcfromtimestamp(raw_timestamp))
+
+                cont_type, cont_length = carver.get_content_info(BytesIO(payload))
+                if all([cont_type, cont_length]):
+                    Vault.add_carving_queue(k, timestamp, cont_type, cont_length)
+
                 try:
-                    if (matches := self._rules.match(data=payload)):
-                        raw_timestamp = Escapy.convert_packet(stream[0], "Timestamp")
-                        timestamp = str(datetime.datetime.utcfromtimestamp(raw_timestamp))
+                    if (matches := Yara._rules.match(data=payload)):
 
                         if "url" in matches[0].rule:
-                            self.url_yar(stream, k, payload, matches, timestamp)
+                            Yara.url_yar(stream, k, payload, matches, timestamp)
 
                         Organize.add_stream_entry(k, stream, payload, matches, timestamp)
                 except AttributeError:
                     logger.critical(f"Yara rules error, check rules in \"rules/custom_rules/<file>\" [{Thread.name()}]")
 
     '''
-    function "url_yar(self, stream, k, matches)" 
+    function "url_yar(, stream, k, matches)" 
 
     E.g. 
     When there is a URL in an email (or in any stream payload), it will search through the "suspicious" or "malicious" urls/ips specified in threat_intel's yara rules, if matched, flag it
 
     '''
 
-    def url_yar(self, stream, k, payload, matches, timestamp):
+    @staticmethod
+    def url_yar(stream, k, payload, matches, timestamp):
         for url in matches[0].strings:
-            if (matches := self._url_rules.match(data=url[2])):
+            if (matches := Yara._url_rules.match(data=url[2])):
                 Organize.add_stream_entry(k, stream, payload, matches, timestamp)
+
+
+    @staticmethod
+    def scan_carved(k, timestamp, payload):
+            try:
+                if (matches := Yara._rules.match(data=payload)):
+
+                    if "url" in matches[0].rule:
+                        Yara.url_yar(None, k, payload, matches, timestamp)
+
+                    Organize.add_stream_entry(k, None, payload, matches, timestamp)
+            except AttributeError:
+                logger.critical(f"Yara rules error, check rules in \"rules/custom_rules/<file>\" [{Thread.name()}]")
+
 
 
 if __name__ == "__main__":
