4f3378a3e74417f88c28b1c9d5dbbdccd6f038e2
chong00 <chongpeng1996@gmail.com>
2020-10-20 20:45:20 +0800

    Fix decode

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -34,28 +34,29 @@ $(document).ready(function (){
             
     });
 
-    $("#viewflagged").click(function(){
-        var btn = document.getElementById("save");
-        var save = btn.innerHTML;
-        console.log(save);
-        if (btn.innerHTML == "Save"){
-            console.log("change to stop saving");
-            btn.innerHTML = "Stop Saving";
-        }else{
-            console.log("change to Save");
-            btn.innerHTML = "Save";
-        }
-        var a = {'data':save};
+    $(".payload").click(function(){
+        var a = {'data':$(this).val()};
         $.ajax({
+
             type:'post',
-            url:'/save',
+            url:'/flagged',
             contentType: "application/json",
             data:JSON.stringify(a),
             success:function(data){
+                document.getElementById("payload").innerHTML = data;
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) { 
+                alert("Status: " + textStatus); alert("Error: " + errorThrown); 
             }
+
         });  
-            
+
     });
+
+    
+
+
+
 
 });
 
